76eb44cf58825f50c707824ded5606476918d44e
miskat433 <miskat433@gmail.com>
2022-09-02 23:25:08 +0600

    main top section add

--- js/main.js
+++ js/main.js
@@ -6,15 +6,27 @@ fetch("https://openapi.programming-hero.com/api/news/categories")
         console.log(err.message);
     })
 
+
+// thumbnail item add
 const newsThumbnail = (tnumbnails) => {
+
     const findNewsThumbnail = document.getElementById("newsThumbnail");
     tnumbnails.forEach(element => {
         const li = document.createElement("li")
         li.innerHTML = `
         <a href="#" class="block py-2 pr-4 pl-3 text-gray-500 hover:text-teal-700 text-lg rounded md:bg-transparent "
         aria-current="page" id=thumbnail-${element.category_id}>${element.category_name}</a>
-        `
+        `;
+        // find thumbnail
         findNewsThumbnail.appendChild(li)
+        document.getElementById(`thumbnail-${element.category_id}`).addEventListener("click", (e) => {
+            const selectItem = document.getElementById('selectItem')
+            selectItem.innerText = e.target.innerText
+            e.target.style.backgroundColor = "teal"
+            e.target.style.color = "white";
+            console.log(e.target.innerText);
+
+        })
     });
 
 

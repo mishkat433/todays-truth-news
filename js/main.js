41a3e52b8fdebbd5b24727240202fc0bbb99b568
miskat433 <miskat433@gmail.com>
2022-09-03 13:31:09 +0600

    some modal bug fix

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 // thumbnail load
 fetch("https://openapi.programming-hero.com/api/news/categories")
     .then(res => res.json())
-    .then(thumbnails => newsThumbnail(thumbnails.data.news_category))
+    .then(thumbnails => {
+        newsThumbnail(thumbnails.data.news_category)
+    })
     .catch(err => {
         console.log(err.message);
     });
+
 
 
 // thumbnail item add
@@ -21,13 +24,11 @@ const newsThumbnail = (tnumbnails) => {
         // find thumbnail
         findNewsThumbnail.appendChild(li)
         findElement(`${element.category_id}`).addEventListener("click", (e) => {
-            // e.target.style.backgroundColor = "teal"
-            // e.target.style.color = "white";
             newsLoader(e.target.id, e.target.innerText);
-            spinner(true)
+            spinner(true);
         })
     });
-    findElement("dataCount").innerText = "data not found"
+    // findElement("dataCount").innerText = "data not found"
 }
 
 
@@ -45,11 +46,11 @@ const newsLoader = async (id, category) => {
     }
 }
 
-// news add
+
+// display news
 const displayAllNews = (cartItems, category) => {
 
-    findElement("dataCount").innerText = cartItems.length !== 0 && category ? `${cartItems.length} items found for ${category} category` : "news not found";
-
+    findElement("dataCount").innerText = cartItems.length !== 0 || category ? `${cartItems.length} items found for ${category} category` : "news not found";
     spinner(false)
 
     // const arr = []
@@ -58,99 +59,84 @@ const displayAllNews = (cartItems, category) => {
 
     const allNews = findElement("allNews");
     allNews.innerHTML = '';
-    cartItems.forEach(items => {
 
-        const div = document.createElement("div");
+    if (cartItems.length !== 0) {
+        cartItems.forEach(items => {
+            const div = document.createElement("div");
 
-        div.classList.add('flex', 'flex-col', 'justify-left', 'items-center', 'w-full', 'bg-teal-50', 'rounded-lg', 'border', 'shadow-md', 'md:flex-row', 'mb-5', 'gap-5')
-        div.innerHTML = `
-        <img class= "w-4/12 ml-2 h-full" src = "${items.image_url ? items.image_url : "image not found"}" alt = "" >
-        <label for="my-modal" class="cursor-pointer" onclick=newsDetails('${items._id}')>
-            <div class="flex flex-col justify-between p-4 leading-normal" >
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Noteworthy
-            technology acquisitions 2021</h5>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">${items.title ? items.title : "no data found"}</p>
-
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">${items.details ? `${items.details.slice(0, 400)}...` : "not found"
-            }</p >
-    
-    <div class="flex justify-between items-center mt-5">
-        <div class="flex items-center">
-            <img class="w-12 rounded-full mr-3" src="${items.author.img ? items.author.img : " no found"} " alt="">
-            <div class="">
-                <h4 class="font-semibold">${items.author.name ? items.author.name : "name not found"}</h4>
-                <p class="text-gray-500">${items.author.published_date ? items.author.published_date : "not found"} </p>
+            div.classList.add('flex', 'flex-col', 'justify-left', 'items-center', 'w-full', 'bg-teal-50', 'rounded-lg', 'border', 'shadow-md', 'md:flex-row', 'mb-5', 'gap-5')
+            div.innerHTML = `
+            <img class= "w-4/12 ml-2 h-full" src = "${items.image_url ? items.image_url : "image not found"}" alt = "" >
+            <label for="my-modal" class="cursor-pointer" onclick=newsDetails('${items._id}')>
+                <div class="flex flex-col justify-between p-4 leading-normal" >
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Noteworthy
+                technology acquisitions 2021</h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">${items.title ? items.title : "no data found"}</p>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">${items.details ? `${items.details.slice(0, 400)}...` : "not found"
+                }</p >
+        
+        <div class="flex justify-between items-center mt-5">
+            <div class="flex items-center">
+                <img class="w-12 rounded-full mr-3" src="${items.author.img ? items.author.img : " no found"} " alt="">
+                <div class="">
+                    <h4 class="font-semibold">${items.author.name ? items.author.name : "name not found"}</h4>
+                    <p class="text-gray-500">${items.author.published_date ? items.author.published_date : "not found"} </p>
+                </div>
+            </div>
+            <div class="flex items-center gap-2">
+                <i class="fa-regular fa-eye text-teal-500"></i>
+                <p><span>${items.total_view ? items.total_view : "not found"}</span> M</p>
+            </div>
+            <div class="text-teal-500">
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+            </div>
+            <div>
+                <i class="fa-solid fa-arrow-right-long font-2xl text-teal-700"></i>
             </div>
         </div>
-        <div class="flex items-center gap-2">
-            <i class="fa-regular fa-eye text-teal-500"></i>
-            <p><span>${items.total_view ? items.total_view : "not found"}</span> M</p>
-        </div>
-        <div class="text-teal-500">
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-regular fa-star"></i>
-        </div>
-        <div>
-            <i class="fa-solid fa-arrow-right-long font-2xl text-teal-700"></i>
-        </div>
-    </div>
-    </ >
-    </label >
-    `
-        allNews.appendChild(div)
-        findElement("navImage").setAttribute("src", `${items.author.img} `)
-    });
-
+        </ >
+        </label >
+        `
+            allNews.appendChild(div)
+            findElement("navImage").setAttribute("src", `${items.author.img} `)
+        });
+    }
+    else {
+        const p = document.createElement("p")
+        p.classList.add('text-center', 'text-2xl', 'text-red-500')
+        p.innerText = "No data found";
+        allNews.appendChild(p)
+    }
 }
 newsLoader("08")
 
 
 
 // details news load
-const newsDetails = async (id) => {
-    try {
-        const res = await fetch(`https://openapi.programming-hero.com/api/news/${id}`)
-        const findNews = await res.json()
-        displayNewsDetails(findNews.data[0])
-    }
-    catch (err) {
-        console.log(err.message);
-    }
-
+const newsDetails = (id) => {
+    fetch(`https://openapi.programming-hero.com/api/news/${id}`)
+        .then(res => res.json())
+        .then(findNews => displayNewsDetails(findNews.data[0]))
+        .catch(err => {
+            err.message
+        })
 }
-
-
-
-
-
-
-
-
 
 
 // Display news details 
 const displayNewsDetails = (details) => {
-    console.log(details);
-    const findDetailsNews = findElement("detailsNews")
-    const div = document.createElement("div")
 
-    div.innerHTML = `
-    <input type="checkbox" id="my-modal" class="modal-toggle" />
-    <div class="modal">
-        <div class="modal-box">
-            <img w-full src="${details.image_url ? details.image_url : "image not found"}" alt="">
-            <h3 class="font-bold text-lg">${details.title ? details.title : "not found"} </h3>
-            <p class="py-4 text-justify">${details.details ? details.details : "not found"}</p>
-            <div class="modal-action">
-                <label for="my-modal" class="bg-teal-500 cursor-pointer rounded-lg py-3 px-4 text-white">Close</label>
-            </div>
-        </div>
-    </div>
-    `
-    findDetailsNews.appendChild(div)
+    findElement("image").setAttribute("src", details.image_url);
+
+    findElement("newsTitle").innerText = details.title ? details.title : "not found";
+    findElement("newsDetails").innerText = details.details ? details.details : "not found";
+    findElement("newsTitle").innerText = details.title ? details.title : "not found";
+
+
 
 }
 
@@ -161,7 +147,8 @@ const displayNewsDetails = (details) => {
 const spinner = (condition) => {
     const findSpinner = findElement("spinner");
     if (condition) {
-        findSpinner.style.display = ("block")
+        findSpinner.style.display = ("block");
+        findElement("allNews").innerHTML = "";
     }
     else {
         findSpinner.style.display = ("none")
@@ -170,7 +157,6 @@ const spinner = (condition) => {
 
 
 // common function
-
 const findElement = (elementId) => {
     const element = document.getElementById(elementId)
     return element
